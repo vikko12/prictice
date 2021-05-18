f42823360989d88705002411baa4602fc3b83cc4
vikkowang <1218297052@qq.com>
2021-05-18 20:19:23 +0800

    populate-8

---
+++ src/main/java/com/vikko/demo/suanfa/year2021/month5/BSTIterator.java
@@ -0,0 +1,32 @@
+package com.vikko.demo.suanfa.year2021.month5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BSTIterator {
+    private int idx;
+    private List<Integer> arr;
+
+    public BSTIterator(TreeNode root) {
+        idx = 0;
+        arr = new ArrayList<Integer>();
+        inorderTraversal(root, arr);
+    }
+
+    public int next() {
+        return arr.get(idx++);
+    }
+
+    public boolean hasNext() {
+        return idx < arr.size();
+    }
+
+    private void inorderTraversal(TreeNode root, List<Integer> arr) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, arr);
+        arr.add(root.val);
+        inorderTraversal(root.right, arr);
+    }
+}
---
+++ src/main/java/com/vikko/demo/suanfa/year2021/month5/SumNumbers.java
@@ -0,0 +1,30 @@
+package com.vikko.demo.suanfa.year2021.month5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: vikko
+ * @Date: 2021/5/18 16:10
+ * @Description:
+ */
+public class SumNumbers {
+
+	private int sum = 0;
+	public int sumNumbers(TreeNode root) {
+		helper(root, 0);
+		return sum;
+	}
+	public void helper(TreeNode root, int res) {
+		if (root == null) {
+			return;
+		}
+		res = (res * 10) + root.val;
+		if (root.left == null && root.right == null) {
+			sum += res;
+		}
+		helper(root.left, res);
+		helper(root.right, res);
+	}
+
+}
