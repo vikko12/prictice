1a79154af2554130456502a5002683984bfc116b
vikkowang <1218297052@qq.com>
2021-05-25 18:28:40 +0800

    populate-11

--- src/main/java/com/vikko/demo/suanfa/year2021/month5/DailyTemperatures.java
+++ src/main/java/com/vikko/demo/suanfa/year2021/month5/DailyTemperatures.java
@@ -1,12 +1,14 @@
 package com.vikko.demo.suanfa.year2021.month5;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
 /**
  * @author: vikko
  * @Date: 2021/5/19 19:15
- * @Description:
+ * @Description: 单调栈 每日温度
  */
 public class DailyTemperatures {
 
@@ -25,4 +27,22 @@ public class DailyTemperatures {
 		return ans;
 	}
 
+
+	public static int[] test(int[] temperatures) {
+		int[] res = new int[temperatures.length];
+		Deque<Integer> deque = new ArrayDeque<>();
+		for (int i = 0; i < temperatures.length; i++) {
+			while (!deque.isEmpty() && temperatures[deque.peek()] < temperatures[i]) {
+				res[deque.peek()] = i - deque.pop();
+			}
+			deque.push(i);
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {73,74,75,71,69,72,76,73};
+		int[] test = test(a);
+		System.out.println(Arrays.toString(test));
+	}
 }
