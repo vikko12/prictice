73407a9fd18fc504655bd88ae569d34fe78fe2a1
vikkowang <1218297052@qq.com>
2021-08-05 19:09:24 +0800

    populate

--- src/main/java/com/vikko/demo/algorithm/year2021/month7/IsValid.java
+++ src/main/java/com/vikko/demo/algorithm/year2021/month7/IsValid.java
@@ -1,5 +1,10 @@
 package com.vikko.demo.algorithm.year2021.month7;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: vikko
  * @Date: 2021/7/30 18:01
@@ -8,12 +13,29 @@ package com.vikko.demo.algorithm.year2021.month7;
 public class IsValid {
 
 	public static void main(String[] args) {
-
+		boolean valid = isValid("([)]");
 	}
 
 	public static boolean isValid(String s) {
 
-		return false;
+		Deque<Character> deque = new ArrayDeque<>();
+		char[] chars = s.toCharArray();
+		Map<Character, Character> map = new HashMap<>();
+		map.put('[',']');
+		map.put('{','}');
+		map.put('(',')');
+		for (char cur : chars) {
+			if(map.containsKey(cur)){
+				deque.offerFirst(cur);
+			}else {
+				if(!deque.isEmpty() && map.get(deque.peek()) == cur){
+					deque.pop();
+				}else {
+					return false;
+				}
+			}
+		}
+		return deque.isEmpty();
 	}
 
 }
