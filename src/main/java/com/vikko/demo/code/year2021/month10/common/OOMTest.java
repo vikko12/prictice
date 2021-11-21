0e12785d8ed3ee3e9288889b10ecc104d97e83b6
vikkowang <1218297052@qq.com>
2021-11-21 16:41:07 +0800

    commit

---
+++ src/main/java/com/vikko/demo/code/year2021/month10/common/OOMTest.java
@@ -0,0 +1,52 @@
+package com.vikko.demo.code.year2021.month10.common;
+
+import com.vikko.demo.code.Test;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: vikko
+ * @Date: 2021/11/21 15:03
+ * @Description:
+ */
+public class OOMTest {
+
+	private void dontStop() {
+		while(true) {
+		}
+	}
+
+	public void stackLeakByThread() {
+		while(true) {
+			Thread thread=new Thread(new Runnable() {
+				public void run() {
+					dontStop();
+				}
+			});
+			thread.start();
+		}
+	}
+
+	public static void main(String [] args) throws Throwable{
+		OOMTest oom=new OOMTest();
+		oom.stackLeakByThread();
+	}
+
+
+	@Data
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	static class User{
+		private Integer age;
+		private String name;
+		private Long birth;
+
+		public User(Long birth) {
+			this.birth = birth;
+		}
+	}
+
+}
